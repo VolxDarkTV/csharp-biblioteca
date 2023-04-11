f5bfa0db14fa58a8dbda7af9b34460fefc6f5555
Vincenzo FOTI <vincenzo.foti@outlook.it>
2023-04-11 22:57:46 +0200

    add user name search

--- csharp-biblioteca/Library.cs
+++ csharp-biblioteca/Library.cs
@@ -11,44 +11,12 @@ namespace csharp_biblioteca
     internal class Library
     {
 
-        //public void LibraryNameSearch() 
-        //{
-
-        //    User u1 = new User("vincenzo", "FOTI", "vincewnzo.foti02@gmail.coms", "password", "+39 3664248926");
-        //    User u2 = new User("giGi", "FOTI", "vincewnzo.foti@gmail.com", "password", "+39 3664248926");
-        //    User u3 = new User("test", "FOTI", "vincewnzo.foti@gmail.com", "password", "+39 3664248926");
-        //    User u4 = new User("vincenzo", "FOTI", "vincewnzo.foti04@gmail.com", "password", "+39 3664248926");
-        //    List<User> users = new List<User>();
-        //    users.Add(u1);
-        //    users.Add(u2);
-        //    users.Add(u3);
-        //    users.Add(u4);
-
-        //    Console.WriteLine("Ricerca x nome:");
-        //    string nameToFind = Console.ReadLine();
-
-        //    List<User> usersWithName = users.Where(x => x.Name.ToLower() == nameToFind.ToLower()).ToList();
-
-        //    if (usersWithName.Count > 0)
-        //    {
-        //        Console.WriteLine($"Users with name '{nameToFind}':");
-        //        foreach (User user in usersWithName)
-        //        {
-        //            Console.WriteLine($"{user.Name} {user.Last} {user.Email}");
-        //        }
-        //    }
-        //    else
-        //    {
-        //        Console.WriteLine($"No users found with name '{nameToFind}'.");
-        //    }
-
-        //}
-
         public List<Document> Documents = new List<Document>();
 
         public void LibraryBookSearch()
         {
             
+
             //Set instance
             Book b1 = new Book("Balena", 2002, "Narrative", 25, "judsb", 400);
             Book b2 = new Book("pippo", 2002, "Narrative", 25, "judsb", 400);
@@ -117,10 +85,36 @@ namespace csharp_biblioteca
                 Console.WriteLine($"No books found with title '{nameToFind}'.");
             }
 
-            
 
         }
 
+        public void LibraryNameSearch() 
+        
+            User u1 = new User("vincenzo", "FOTI", "vincewnzo.foti02@gmail.coms", "password", "+39 3664248926");
+            User u2 = new User("giGi", "FOTI", "vincewnzo.foti@gmail.com", "password", "+39 3664248926");
+            User u3 = new User("test", "FOTI", "vincewnzo.foti@gmail.com", "password", "+39 3664248926");
+            User u4 = new User("vincenzo", "FOTI", "vincewnzo.foti04@gmail.com", "password", "+39 3664248926");
+            List<User> users = new List<User>();
+            users.Add(u1);
+            users.Add(u2);
+            users.Add(u3);
+            users.Add(u4
+            Console.WriteLine("Ricerca x nome:");
+            string nameToFind = Console.ReadLine
+            List<User> usersWithName = users.Where(x => x.Name.ToLower() == nameToFind.ToLower()).ToList
+            if (usersWithName.Count > 0)
+            {
+                Console.WriteLine($"Users with name '{nameToFind}':");
+                foreach (User user in usersWithName)
+                {
+                    Console.WriteLine($"{user.Name} {user.Last} {user.Email}");
+                }
+            }
+            else
+            {
+                Console.WriteLine($"No users found with name '{nameToFind}'.");
+            
+        }
 
 
     }
